094c618d12db2a8f93a75d0fcd6cc205f5c21bde
Yoshi <dleonavichus@mail.ru>
2023-10-04 10:38:22 +0300

    lab2, fix lab1

--- lab2/array.c
+++ lab2/array.c
@@ -1,25 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+// Функция для записи времени выполнения в файл и вывода на консоль
+void logExecutionTime(const char* functionName, clock_t start_time) {
+    clock_t end_time = clock();
+    double execution_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+    printf("Время выполнения %s: %lf секунд\n", functionName, execution_time);
+    
+    FILE* time_file = fopen("times.txt", "a");
+    if (time_file == NULL) {
+        printf("Ошибка открытия файла для записи времени.\n");
+        exit(1);
+    }
+    fprintf(time_file, "%s: %lf секунд\n", functionName, execution_time);
+    fclose(time_file);
+}
 
 // Функция для создания динамического массива указанного пользователем размера
 int* createArray(int size) {
+    clock_t start_time = clock();
     int* array = (int*)malloc(size * sizeof(int));
     if (array == NULL) {
         printf("Ошибка выделения памяти для массива.\n");
         exit(1);
     }
+    logExecutionTime("createArray", start_time);
     return array;
 }
 
 // Функция для заполнения массива индексами
 void fillArrayWithIndices(int* array, int size) {
+    clock_t start_time = clock();
     for (int i = 0; i < size; i++) {
         array[i] = i;
     }
+    logExecutionTime("fillArrayWithIndices", start_time);
 }
 
 // Функция для вывода содержимого массива в заданном диапазоне
 void printArrayInRange(int* array, int start, int end) {
+    clock_t start_time = clock();
     if (start < 0 || start > end) {
         printf("Некорректный диапазон.\n");
         return;
@@ -29,10 +50,12 @@ void printArrayInRange(int* array, int start, int end) {
         printf("%d ", array[i]);
     }
     printf("\n");
+    logExecutionTime("printArrayInRange", start_time);
 }
 
 // Функция для сохранения массива в двоичный файл
 void saveArrayToBinaryFile(int* array, int size, const char* filename) {
+    clock_t start_time = clock();
     FILE* file = fopen(filename, "wb");
     if (file == NULL) {
         printf("Ошибка открытия файла для записи.\n");
@@ -40,10 +63,12 @@ void saveArrayToBinaryFile(int* array, int size, const char* filename) {
     }
     fwrite(array, sizeof(int), size, file);
     fclose(file);
+    logExecutionTime("saveArrayToBinaryFile", start_time);
 }
 
 // Функция для считывания содержимого массива из двоичного файла и вывода в консоль
 void readArrayFromBinaryFile(int** array, int* size, const char* filename) {
+    clock_t start_time = clock();
     FILE* file = fopen(filename, "rb");
     if (file == NULL) {
         printf("Ошибка открытия файла для чтения.\n");
@@ -62,6 +87,8 @@ void readArrayFromBinaryFile(int** array, int* size, const char* filename) {
     
     fread(*array, sizeof(int), *size, file);
     fclose(file);
+    
+    logExecutionTime("readArrayFromBinaryFile", start_time);
 }
 
 int main() {
@@ -96,4 +123,3 @@ int main() {
 
     return 0;
 }
-
