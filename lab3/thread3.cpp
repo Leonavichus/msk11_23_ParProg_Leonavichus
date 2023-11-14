76f821fab5f93431baabb4381c57dbfd4c1f1d27
Yoshi <dleonavichus@mail.ru>
2023-11-14 21:37:58 +0300

    Minor fix

--- lab3/thread3.cpp
+++ lab3/thread3.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <thread>
+#include <numeric>
 
-// Функция для вычисления скалярного произведения для определенного участка векторов
-int scalarProduct(const std::vector<int> &v1, const std::vector<int> &v2, int start, int end)
+using namespace std;
+
+// Функция для вычисления скалярного произведения векторов
+size_t partialDotProduct(const vector<int> &v1, const vector<int> &v2, size_t start, size_t end)
 {
-    int result = 0;
-    for (int i = start; i < end; ++i)
+    size_t result = 0;
+    for (size_t i = start; i < end; ++i)
     {
         result += v1[i] * v2[i];
     }
@@ -15,22 +18,37 @@ int scalarProduct(const std::vector<int> &v1, const std::vector<int> &v2, int st
 
 int main()
 {
-    std::vector<int> vector1 = {1, 2, 3, 4, 5};
-    std::vector<int> vector2 = {6, 7, 8, 9, 10};
+    // Инициализация векторов
+    vector<int> vector1 = {1, 2, 3, 4, 5};
+    vector<int> vector2 = {6, 7, 8, 9, 10};
 
-    int numThreads = 5;
-
-    std::vector<std::thread> threads;
-    std::vector<int> results(numThreads, 0);
-
-    // Запуск потоков для вычисления скалярного произведения
-    for (int i = 0; i < numThreads; ++i)
+    // Проверка на одинаковую длину векторов
+    if (vector1.size() != vector2.size())
     {
-        threads.emplace_back(std::thread([&vector1, &vector2, &results, i, numThreads]()
-                                         {
-            int start = i * (vector1.size() / numThreads);
-            int end = (i + 1) * (vector1.size() / numThreads);
-            results[i] = scalarProduct(vector1, vector2, start, end); }));
+        cerr << "Ошибка: Векторы должны быть одинаковой длины." << endl;
+        return 1;
+    }
+
+    // Получение количества потоков, которые могут быть использованы
+    const size_t numThreads = thread::hardware_concurrency();
+
+    // Вычисление размера блока для каждого потока
+    const size_t blockSize = vector1.size() / numThreads;
+
+    // Вектор для хранения результатов вычислений каждого потока
+    vector<int> partialResults(numThreads);
+
+    // Вектор потоков
+    vector<thread> threads;
+
+    // Запуск потоков
+    for (size_t i = 0; i < numThreads; ++i)
+    {
+        threads.emplace_back(thread([&vector1, &vector2, &partialResults, i, numThreads, blockSize]()
+                                    {
+            size_t start = i * blockSize;
+            size_t end =  (i == numThreads - 1) ? vector1.size() : start + blockSize;
+            partialResults[i] = partialDotProduct(vector1, vector2, start, end); }));
     }
 
     // Ожидание завершения всех потоков
@@ -39,14 +57,11 @@ int main()
         thread.join();
     }
 
-    // Суммирование результатов
-    int finalResult = 0;
-    for (auto &res : results)
-    {
-        finalResult += res;
-    }
+    // Суммируем результаты из всех потоков
+    int finalResult = accumulate(partialResults.begin(), partialResults.end(), 0);
 
-    std::cout << "Скалярное произведение векторов: " << finalResult << std::endl;
+    // Выводим результат
+    cout << "Скалярное произведение векторов: " << finalResult << endl;
 
     return 0;
 }
